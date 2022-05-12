a2aba5fbdf09d9897dd943f4c08cf7d24a8bdbcf
Ronaldolvf <101340502+Ronaldolvf@users.noreply.github.com>
2022-05-12 16:18:13 -0300

    Update adivinhaPalavra.py

--- adivinhaPalavra.py
+++ adivinhaPalavra.py
@@ -63,7 +63,7 @@ def jogar():        # É uma função
 
 
 def mensagens_de_boas_vindas():  # Criar uma função para deixar o cógigo mais organizado
-    print ("Bem vindo")
+    print ("Bem vindooooooooooooooooooooooooooooooooooooooo")
 
 
 def esse_e_o_chute():
